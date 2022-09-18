74cfb18adc84007efde675385c58d5a8e071c855
altenci <altenci0812@gmail.com>
2022-09-18 17:30:54 +0800

    first commit

---
+++ resources/views/pages/sview.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First View</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+
+</head>
+<body>
+    <div>
+        <h1>This is the Second View</h1>
+        <a href="{{url('/')}}">First View</a>
+        <a href="{{url('/tview')}}">Third View</a>
+    </div>
+</body>
+</html>
